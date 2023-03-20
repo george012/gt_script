15408ed7205f60faacf211259fcb4097064a0dcf
TaoTao <george012@163.com>
2023-03-20 18:24:01 +0800

    优化TCP keepalive

--- optimize_network.sh
+++ optimize_network.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-optimize_network(){
+optimize_network() {
     # 优化系统配置以支持更高的并发连接
     # 提高文件描述符限制
     echo "fs.file-max = 1000000" | sudo tee -a /etc/sysctl.conf
@@ -13,9 +13,14 @@ optimize_network(){
 
     # 启用 TCP Selective Acknowledgments（SACK）
     echo "net.ipv4.tcp_sack = 1" | sudo tee -a /etc/sysctl.conf
-    
+
     # 启用 TCP 时间戳
     echo "net.ipv4.tcp_timestamps = 1" | sudo tee -a /etc/sysctl.conf
+
+    # TCP keepalive 参数
+    echo "net.ipv4.tcp_keepalive_time = 300" | sudo tee -a /etc/sysctl.conf
+    echo "net.ipv4.tcp_keepalive_probes = 5" | sudo tee -a /etc/sysctl.conf
+    echo "net.ipv4.tcp_keepalive_intvl = 15" | sudo tee -a /etc/sysctl.conf
 
     # 应用更改
     sudo sysctl -p
