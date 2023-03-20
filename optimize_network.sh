dd6f0b5a3f5b2d0a11f0f84ee75dc52adcdd258a
TaoTao <george012@163.com>
2023-03-20 23:18:06 +0800

    no message

--- optimize_network.sh
+++ optimize_network.sh
@@ -1,33 +1,75 @@
 #!/bin/bash
 
-optimize_network() {
-    # 优化系统配置以支持更高的并发连接
-    # 提高文件描述符限制
-    echo "fs.file-max = 1000000" | sudo tee -a /etc/sysctl.conf
+optimize_limits_conf() {
+    local limits_conf_file="/etc/security/limits.conf"
+    local limits_conf=(
+        "* soft nofile 2097152"
+        "* hard nofile 2097152"
+        "* soft nproc 51200"
+        "* hard nproc 51200"
+    )
 
-    # 提高网络连接限制
-    echo "net.core.somaxconn = 65535" | sudo tee -a /etc/sysctl.conf
-    echo "net.ipv4.tcp_max_syn_backlog = 65535" | sudo tee -a /etc/sysctl.conf
-    echo "net.ipv4.ip_local_port_range = 1024 65535" | sudo tee -a /etc/sysctl.conf
-    echo "net.ipv4.tcp_tw_reuse = 1" | sudo tee -a /etc/sysctl.conf
+    if [ ! -f "$limits_conf_file" ]; then
+        echo "File not found: $limits_conf_file"
+        exit 1
+    fi
 
-    # 启用 TCP Selective Acknowledgments（SACK）
-    echo "net.ipv4.tcp_sack = 1" | sudo tee -a /etc/sysctl.conf
+    if [ ! -f "$limits_conf_file.bak" ]; then
+        cp "$limits_conf_file" "$limits_conf_file.bak"
+    fi
 
-    # 启用 TCP 时间戳
-    echo "net.ipv4.tcp_timestamps = 1" | sudo tee -a /etc/sysctl.conf
+    for conf_item in "${limits_conf[@]}"; do
+        local item_name=$(echo "$conf_item" | awk '{print $2}')
+        local item_value=$(echo "$conf_item" | awk '{print $4}')
+        local item_type=$(echo "$conf_item" | awk '{print $3}')
 
-    # TCP keepalive 参数
-    echo "net.ipv4.tcp_keepalive_time = 300" | sudo tee -a /etc/sysctl.conf
-    echo "net.ipv4.tcp_keepalive_probes = 5" | sudo tee -a /etc/sysctl.conf
-    echo "net.ipv4.tcp_keepalive_intvl = 15" | sudo tee -a /etc/sysctl.conf
+        if grep -q "$item_name" "$limits_conf_file"; then
+            sudo sed -i "/$item_name/d" "$limits_conf_file"
+        fi
 
-    # 应用更改
-    sudo sysctl -p
-
-    # 设置文件描述符限制
-    echo "* soft nofile 204800" | sudo tee -a /etc/security/limits.conf
-    echo "* hard nofile 204800" | sudo tee -a /etc/security/limits.conf
+        echo "$conf_item" | sudo tee -a "$limits_conf_file" >/dev/null
+    done
 }
 
-optimize_network && wait && history -c
+optimize_sysctl_conf() {
+    local sysctl_conf_file="/etc/sysctl.conf"
+    local sysctl_conf=(
+        "fs.file-max = 2097152"
+        "net.core.somaxconn = 65535"
+        "net.ipv4.tcp_max_syn_backlog = 65535"
+        "net.ipv4.ip_local_port_range = 1024 65535"
+        "net.ipv4.tcp_tw_reuse = 1"
+        "net.ipv4.tcp_sack = 1"
+        "net.ipv4.tcp_timestamps = 1"
+        "net.ipv4.tcp_keepalive_time = 120"
+        "net.ipv4.tcp_keepalive_probes = 9"
+        "net.ipv4.tcp_keepalive_intvl = 10"
+        "net.ipv4.tcp_fin_timeout = 30"
+        "net.ipv4.tcp_tw_recycle = 1"
+    )
+
+    if [ ! -f "$sysctl_conf_file" ]; then
+        echo "File not found: $sysctl_conf_file"
+        exit 1
+    fi
+
+    if [ ! -f "$sysctl_conf_file.bak" ]; then
+        cp "$sysctl_conf_file" "$sysctl_conf_file.bak"
+    fi
+
+    for conf_item in "${sysctl_conf[@]}"; do
+        local param_name=$(echo "$conf_item" | awk '{print $1}')
+        local param_value=$(echo "$conf_item" | awk '{print $3}')
+
+        if grep -q "^$param_name" "$sysctl_conf_file"; then
+            sudo sed -i "s/^$param_name.*/$conf_item/" "$sysctl_conf_file"
+        else
+            echo "$conf_item" | sudo tee -a "$sysctl_conf_file" >/dev/null
+        fi
+    done
+
+    sudo sysctl -p >/dev/null
+}
+
+optimize_limits_conf && optimize_sysctl_conf
+echo "Optimization complete."
