ac5b4c41fb3397a872791488cfaea76f0a66940a
TaoTao <george012@163.com>
2023-03-20 18:12:30 +0800

    网络优化脚本

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# gt_script
-custom script
+<!-- TOC -->
+
+- [1. use`optimize_network`优化至承载51200并发](#1-useoptimize_network优化至承载51200并发)
+
+<!-- /TOC -->
+# 1. use`optimize_network`优化至承载51200并发
+wget --no-check-certificate https://raw.githubusercontent.com/george012/george012/master/optimize_network.sh && chmod a+x ./optimize_network.sh && ./optimize_network.sh
---
+++ optimize_network.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+optimize_network(){
+    # 优化系统配置以支持更高的并发连接
+    # 提高文件描述符限制
+    echo "fs.file-max = 1000000" | sudo tee -a /etc/sysctl.conf
+
+    # 提高网络连接限制
+    echo "net.core.somaxconn = 65535" | sudo tee -a /etc/sysctl.conf
+    echo "net.ipv4.tcp_max_syn_backlog = 65535" | sudo tee -a /etc/sysctl.conf
+    echo "net.ipv4.ip_local_port_range = 1024 65535" | sudo tee -a /etc/sysctl.conf
+    echo "net.ipv4.tcp_tw_reuse = 1" | sudo tee -a /etc/sysctl.conf
+
+    # 启用 TCP Selective Acknowledgments（SACK）
+    echo "net.ipv4.tcp_sack = 1" | sudo tee -a /etc/sysctl.conf
+    
+    # 启用 TCP 时间戳
+    echo "net.ipv4.tcp_timestamps = 1" | sudo tee -a /etc/sysctl.conf
+
+    # 应用更改
+    sudo sysctl -p
+}
+
+optimize_network && wait && history -c
