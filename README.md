cdc205c7c81d007f54237f49831647b3bce46158
TaoTao <george012@163.com>
2023-03-29 19:09:41 +0800

    add install docker

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 - [1. 关闭ipv6](#1-关闭ipv6)
 - [2. use`optimize_network`优化至承载51200并发](#2-useoptimize_network优化至承载51200并发)
+- [2. use`install_docker`](#2-useinstall_docker)
 
 <!-- /TOC -->
 
@@ -13,4 +14,9 @@ ipv6的现有软件兼容性考虑
 # 2. use`optimize_network`优化至承载51200并发
 ```
 wget --no-check-certificate https://raw.githubusercontent.com/george012/gt_script/master/optimize_network.sh && chmod a+x ./optimize_network.sh && ./optimize_network.sh
+```
+
+# 2. use`install_docker`
+```
+wget --no-check-certificate https://raw.githubusercontent.com/george012/gt_script/master/install_docker.sh && chmod a+x ./install_docker.sh && ./install_docker.sh
 ```
---
+++ install_docker.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+SCRIPT_NAME=$(basename $0)
+
+pre_config(){
+    sudo apt update && wait && sudo apt install -y unzip zip wget logrotate apt-transport-https ca-certificates curl gnupg lsb-release
+}
+
+config_docker_install(){
+    sudo mkdir -m 0755 -p /etc/apt/keyrings \
+    && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg \
+    && echo \
+        "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+        "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+        sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+}
+
+install_docker(){
+    sudo apt update \
+    && wait \
+    && sudo chmod a+r /etc/apt/keyrings/docker.gpg \
+    && wait \
+    && sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+}
+
+
+install(){
+    pre_config && wait && config_docker_install && wait && install_docker && wait && sudo systemctl enable --now docker
+}
+
+install && wait && rm -rf $SCRIPT_NAME
