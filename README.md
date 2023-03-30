06441a1239e764dd312172c7ec08e7df30d58cab
WMYeah-NUC <george012@163.com>
2023-03-30 23:41:52 +0800

    no message

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 - [2. use`optimize_network`51200 concurrent](#2-useoptimize_network51200-concurrent)
 - [3. use`install_docker`](#3-useinstall_docker)
 - [4. use`install_redis`](#4-useinstall_redis)
+- [4. use`get_github_repo_version_manager`（仅仅支持Github）](#4-useget_github_repo_version_manager仅仅支持github)
+  - [Simple:](#simple)
 
 <!-- /TOC -->
 
@@ -26,4 +28,18 @@ wget --no-check-certificate https://raw.githubusercontent.com/george012/gt_scrip
 # 4. use`install_redis`
 ```
 wget --no-check-certificate https://raw.githubusercontent.com/george012/gt_script/master/install_redis.sh && chmod a+x ./install_redis.sh && ./install_redis.sh
+```
+
+# 4. use`get_github_repo_version_manager`（仅仅支持Github）
+*   plase edit `$CURRENT_REPO_URI` `$REMOTE_REPO_URI`
+
+```
+wget --no-check-certificate https://raw.githubusercontent.com/george012/gt_script/master/install_redis.sh && chmod a+x ./install_redis.sh && ./install_redis.sh $CURRENT_REPO_URI $REMOTE_REPO_URI
+```
+
+## Simple:
+*   simple: `$CURRENT_REPO_URI` = `github.com/currenttuser/current_repo`
+*   simple: `$CURRENT_REPO_URI` = `github.com/remoteuser/remote_repo`
+```
+wget --no-check-certificate https://raw.githubusercontent.com/george012/gt_script/master/get_github_repo_version_manager.sh && chmod a+x ./get_github_repo_version_manager.sh && ./get_github_repo_version_manager.sh github.com/currenttuser/current_repo github.com/remoteuser/remote_repo
 ```
---
+++ get_github_repo_version_manager.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+set -e
+SCRIPT_NAME=$(basename $0)
+
+pre_config(){
+    apt update && wait && apt install unzip zip wget logrotate
+}
+
+parse_json(){
+    echo "${1//\"/}" | tr -d '\n' | tr -d '\r' | sed "s/.*$2:\([^,}]*\).*/\1/"
+}
+
+get_repo_version(){
+    local REMOTE_REPO_VERSION=""
+    local LATEST_RELEASE_INFO=$(curl --silent https://api.github.com/repos/$1/releases/latest)
+    if ! echo "$LATEST_RELEASE_INFO" | grep -q "Not Found"; then
+        REMOTE_REPO_VERSION=$(parse_json "$LATEST_RELEASE_INFO" "tag_name")
+    fi
+    echo $REMOTE_REPO_VERSION | tr -d '\r\n'
+}
+
+check_need_build(){
+    remote_version=$2
+    this_version=$1
+    if [[ ${this_version:0:1} != "v" ]]; then
+        echo "true" | tr -d '\r\n'
+        return
+    fi
+
+    remote_version_no_v=${remote_version:1}
+    this_version_no_v=${this_version:1}
+    sorted_versions=$(echo -e "$remote_version_no_v\n$this_version_no_v" | sort -V)
+
+    small_version=$(echo "$sorted_versions" | head -n1)
+    if [ "$remote_version_no_v" != "$small_version" ]; then
+        echo "true" | tr -d '\r\n'
+        return
+    fi
+    echo "false" | tr -d '\r\n'
+}
+
+handle_input(){
+    local current_repo_rul=$1
+    local remote_repo_url=$2
+
+
+    if [[ $current_repo_rul == "" ]] || [[ $current_repo_rul == " " ]] || [[ $remote_repo_url == "" ]] || [[ $remote_repo_url == " " ]]; then
+        echo "{\"code\":-1,\"msg\":\"Input Prarms Error：Prarms Cannot Be Empty\"}"
+        return
+    fi
+
+    if [[ $(echo "$current_repo_rul" | cut -d'/' -f1) != "github.com" ]] || [[ $(echo "$remote_repo_url" | cut -d'/' -f1) != "github.com" ]]; then
+        echo "{\"code\":-1,\"msg\":\"This Script Only github.com is supported\"}"
+        return
+    fi
+
+    local current_repo_suffix=$(echo "$current_repo_rul" | cut -d'/' -f2-)
+    local remote_repo_suffix=$(echo "$remote_repo_url" | cut -d'/' -f2-)
+
+    local current_repo_latest_version=$(get_repo_version "$current_repo_suffix")
+    if [[ $current_repo_latest_version == "" ]] || [[ $current_repo_latest_version == " " ]]; then
+        echo "true" | tr -d '\r\n'
+        return
+    fi
+
+    local remote_repo_latest_version=$(get_repo_version "$remote_repo_suffix")
+
+    check_need_build "$current_repo_latest_version" "$remote_repo_latest_version"
+}
+
+handle_input "$@"
