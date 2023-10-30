9eb133001f5aaeee790498c076964982e5c8ff32
WMYeah_Mac <george012@163.com>
2023-10-30 18:53:04 +0800

    add logrotate

--- auto_ssl.sh
+++ auto_ssl.sh
@@ -12,6 +12,67 @@ FULLCHAIN_NAME=fullchain.cer
 PRIVATE_NAME=private.key
 
 
+
+# logrotate config
+create_logrotate_config(){
+sudo rm -rf /etc/logrotate.d/${INPUT_DOMAIN}
+
+cat << EOF | sudo tee /etc/logrotate.d/${INPUT_DOMAIN}
+$CORE_GETH_LOG_Dir/${INPUT_DOMAIN}.log {
+    hourly
+    rotate 720
+    missingok
+    notifempty
+    compress
+    delaycompress
+    copytruncate
+}
+EOF
+}
+
+# logrotate systemd
+create_logrotate_service(){
+sudo rm -rf /etc/systemd/system/${INPUT_DOMAIN}_logrotate.service
+
+cat << EOF | sudo tee /etc/systemd/system/${INPUT_DOMAIN}_logrotate.service
+[Unit]
+Description=Logrotate for ${INPUT_DOMAIN}
+
+[Service]
+Type=oneshot
+ExecStart=/usr/sbin/logrotate /etc/logrotate.d/${INPUT_DOMAIN}
+EOF
+}
+
+# logrotate systemd timer
+create_logrotate_service_timer(){
+sudo rm -rf /etc/systemd/system/${INPUT_DOMAIN}_logrotate.timer
+
+cat << EOF | sudo tee /etc/systemd/system/${INPUT_DOMAIN}_logrotate.timer
+[Unit]
+Description=Run logrotate for ${INPUT_DOMAIN} every 5 minutes
+
+[Timer]
+OnCalendar=*:0/5
+Persistent=true
+
+[Install]
+WantedBy=timers.target
+EOF
+}
+
+function handle_logrotate() {
+    create_logrotate_config
+    create_logrotate_service
+    create_logrotate_service_timer
+
+    sudo systemctl daemon-reload
+    sudo systemctl enable --now ${INPUT_DOMAIN}_logrotate.timer
+    sudo systemctl enable --now ${INPUT_DOMAIN}_logrotate.service
+    sudo systemctl start --now ${INPUT_DOMAIN}_logrotate.service
+} 
+
+
 pre_config(){
     sudo apt update && wait && sudo apt install -y unzip zip wget curl gnupg lsb-release
 }
@@ -73,19 +134,41 @@ create_nginx_vhost(){
 cat << EOF | sudo tee /etc/nginx/conf.d/$INPUT_DOMAIN.conf
 server {
     listen 80;
-    server_name $INPUT_DOMAIN;
-    root $NGINX_WEB_ROOT/$INPUT_DOMAIN/web_root;
+    server_name ${INPUT_DOMAIN};
+    
+    # 重定向所有 HTTP 请求到 HTTPS
+    return 301 https://$server_name$request_uri;
+}
+
+server {
+    listen 443 ssl;
+    server_name ${INPUT_DOMAIN};
+
+    # SSL 证书和私钥的位置
+    ssl_certificate /nginx_web/${INPUT_DOMAIN}/cert/fullchain.cer;
+    ssl_certificate_key /nginx_web/${INPUT_DOMAIN}/cert/private.key;
+
+    # 强化 HTTPS 设置
+    ssl_protocols TLSv1 TLSv1.1 TLSv1.2 TLSv1.3;
+    ssl_prefer_server_ciphers on;
+    ssl_ciphers ssl_ciphers 'ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256';
+
+
+    root $NGINX_WEB_ROOT/${INPUT_DOMAIN}/web_root;
     index index.php index.html index.htm;
     error_page 400 401 403 404 405 406 407 408 409 410 411 412 413 414 415 416 417 500 501 502 503 504 =200 /404.html;
     
     location ~ /(\.git(/|$)|backup(/|$)|\.DS_Store|\.gitignore) {
         deny all;
     }
+
+    access_log /nginx_web/${INPUT_DOMAIN}/logs/access.log;
+    error_log /nginx_web/${INPUT_DOMAIN}/logs/error.log;
 }
 EOF
 
 systemctl reload nginx
-echo "Nginx virtual host configuration for $INPUT_DOMAIN has been created."
+echo "Nginx virtual host configuration for ${INPUT_DOMAIN} has been created."
 }
 
 install_acme(){
@@ -93,29 +176,29 @@ install_acme(){
 }
 
 request_cert(){
-    mkdir -p $NGINX_WEB_ROOT/$INPUT_DOMAIN/cert
-    mkdir -p $NGINX_WEB_ROOT/$INPUT_DOMAIN/web_root
-    mkdir -p $NGINX_WEB_ROOT/$INPUT_DOMAIN/logs
-    $ACME_HOME/acme.sh --issue -d $INPUT_DOMAIN --webroot $NGINX_WEB_ROOT/$INPUT_DOMAIN/web_root/
+    mkdir -p $NGINX_WEB_ROOT/${INPUT_DOMAIN}/cert
+    mkdir -p $NGINX_WEB_ROOT/${INPUT_DOMAIN}/web_root
+    mkdir -p $NGINX_WEB_ROOT/${INPUT_DOMAIN}/logs
+    $ACME_HOME/acme.sh --issue -d ${INPUT_DOMAIN} --webroot $NGINX_WEB_ROOT/${INPUT_DOMAIN}/web_root/
 }
 
 install_cert(){
-$ACME_HOME/acme.sh --install-cert -d $INPUT_DOMAIN \
+$ACME_HOME/acme.sh --install-cert -d ${INPUT_DOMAIN} \
 --key-file       $NGINX_WEB_ROOT/$INPUT_DOMAIN/cert/$PRIVATE_NAME  \
 --fullchain-file $NGINX_WEB_ROOT/$INPUT_DOMAIN/cert/$FULLCHAIN_NAME \
 --reloadcmd     "service nginx force-reload"
 }
 
 create_acme_service(){
-rm -rf /etc/systemd/system/acme-$INPUT_DOMAIN.service
-cat << EOF | sudo tee /etc/systemd/system/acme-$INPUT_DOMAIN.service
+rm -rf /etc/systemd/system/${INPUT_DOMAIN}_acme.service
+cat << EOF | sudo tee /etc/systemd/system/${INPUT_DOMAIN}_acme.service
 [Unit]
-Description=Run acme.sh to renew SSL certificates for $INPUT_DOMAIN
+Description=Run acme.sh to renew SSL certificates for ${INPUT_DOMAIN}
 After=network.target
 
 [Service]
 Type=oneshot
-ExecStart=$ACME_HOME/acme.sh --cron --home $ACME_HOME --domain $INPUT_DOMAIN
+ExecStart=$ACME_HOME/acme.sh --cron --home $ACME_HOME --domain ${INPUT_DOMAIN}
 
 [Install]
 WantedBy=multi-user.target
@@ -123,14 +206,14 @@ EOF
 }
 
 create_acme_service_timer(){
-rm -rf /etc/systemd/system/acme-$INPUT_DOMAIN.timer
-cat << EOF | sudo tee /etc/systemd/system/acme-$INPUT_DOMAIN.timer
+rm -rf /etc/systemd/system/${INPUT_DOMAIN}_acme.timer
+cat << EOF | sudo tee /etc/systemd/system/${INPUT_DOMAIN}_acme.timer
 [Unit]
-Description=Timer for acme.sh service for $INPUT_DOMAIN
+Description=Timer for acme.sh service for ${INPUT_DOMAIN}
 
 [Timer]
 OnCalendar=*-*-* $AUTO_REFRESH_TIME
-Unit=acme-$INPUT_DOMAIN.service
+Unit=${INPUT_DOMAIN}_acme.service
 Persistent=true
 
 [Install]
@@ -140,9 +223,9 @@ EOF
 
 enable_service(){
     sudo systemctl daemon-reload
-    sudo systemctl enable --now acme-$INPUT_DOMAIN.timer
-    sudo systemctl enable --now acme-$INPUT_DOMAIN.service
-    sudo systemctl start --now acme-$INPUT_DOMAIN.service
+    sudo systemctl enable --now ${INPUT_DOMAIN}_acme.timer
+    sudo systemctl enable --now ${INPUT_DOMAIN}_acme.service
+    sudo systemctl start --now ${INPUT_DOMAIN}_acme.service
 }
 
 onkey_install(){
