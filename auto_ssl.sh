4060cb8c3f1b1ececf4f5ef29390f6bd5e485fad
WMYeah_Mac <george012@163.com>
2024-01-11 02:40:21 +0800

    ws

--- auto_ssl.sh
+++ auto_ssl.sh
@@ -133,38 +133,55 @@ input_email(){
 create_nginx_vhost(){
 cat << EOF | sudo tee /etc/nginx/conf.d/$INPUT_DOMAIN.conf
 server {
-    listen 80;
+    listen       80;
     server_name ${INPUT_DOMAIN};
-    
-    # 重定向所有 HTTP 请求到 HTTPS
-    return 301 https://'$server_name$request_uri';
-}
-
-server {
-    listen 443 ssl;
-    server_name ${INPUT_DOMAIN};
-
-    # SSL 证书和私钥的位置
-    ssl_certificate /nginx_web/${INPUT_DOMAIN}/cert/fullchain.cer;
-    ssl_certificate_key /nginx_web/${INPUT_DOMAIN}/cert/private.key;
-
-    # 强化 HTTPS 设置
-    ssl_protocols TLSv1 TLSv1.1 TLSv1.2 TLSv1.3;
-    ssl_prefer_server_ciphers on;
-    ssl_ciphers 'AEAD-AES256-GCM-SHA384 ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256';
-
 
     root $NGINX_WEB_ROOT/${INPUT_DOMAIN}/web_root;
+
     index index.php index.html index.htm;
     error_page 400 401 403 404 405 406 407 408 409 410 411 412 413 414 415 416 417 500 501 502 503 504 =200 /404.html;
-    
+
     location ~ /(\.git(/|$)|backup(/|$)|\.DS_Store|\.gitignore) {
         deny all;
     }
 
-    access_log /nginx_web/${INPUT_DOMAIN}/logs/access.log;
-    error_log /nginx_web/${INPUT_DOMAIN}/logs/error.log;
+    access_log ${NGINX_WEB_ROOT}/${INPUT_DOMAIN}/logs/access.log;
+    error_log ${NGINX_WEB_ROOT}/${INPUT_DOMAIN}/logs/error.log;
 }
+
+# server {
+#     listen 80;
+#     server_name ${INPUT_DOMAIN};
+    
+#     # 重定向所有 HTTP 请求到 HTTPS
+#     return 301 https://'$server_name$request_uri';
+# }
+
+# server {
+#     listen 443 ssl;
+#     server_name ${INPUT_DOMAIN};
+
+#     # SSL 证书和私钥的位置
+#     ssl_certificate /nginx_web/${INPUT_DOMAIN}/cert/fullchain.cer;
+#     ssl_certificate_key /nginx_web/${INPUT_DOMAIN}/cert/private.key;
+
+#     # 强化 HTTPS 设置
+#     ssl_protocols TLSv1 TLSv1.1 TLSv1.2 TLSv1.3;
+#     ssl_prefer_server_ciphers on;
+#     ssl_ciphers 'AEAD-AES256-GCM-SHA384 ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256';
+
+
+#     root $NGINX_WEB_ROOT/${INPUT_DOMAIN}/web_root;
+#     index index.php index.html index.htm;
+#     error_page 400 401 403 404 405 406 407 408 409 410 411 412 413 414 415 416 417 500 501 502 503 504 =200 /404.html;
+    
+#     location ~ /(\.git(/|$)|backup(/|$)|\.DS_Store|\.gitignore) {
+#         deny all;
+#     }
+
+#     access_log ${NGINX_WEB_ROOT}/${INPUT_DOMAIN}/logs/access.log;
+#     error_log ${NGINX_WEB_ROOT}/${INPUT_DOMAIN}/logs/error.log;
+# }
 EOF
 
 systemctl reload nginx
