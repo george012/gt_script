c62d28000e888671c99aef8f8c7b795770624ccd
M3 <george017@163.com>
2025-01-17 21:55:53 +0800

    dds

--- create_restricted_user.sh
+++ create_restricted_user.sh
@@ -21,12 +21,9 @@ sudo usermod -s /bin/bash $USERNAME
 # 创建用户目录并确保用户是该目录的所有者
 sudo mkdir -p /usr/local/$USERNAME
 sudo chown root:root /usr/local/$USERNAME  # root 拥有该目录
+sudo chmod 755 /usr/local/$USERNAME  # root 拥有并且不可写
 
-# 创建用户自己的文件夹，在 ChrootDirectory 下可以操作
-sudo mkdir -p /usr/local/$USERNAME/home/$USERNAME
-sudo chown $USERNAME:$USERNAME /usr/local/$USERNAME/home/$USERNAME  # 用户拥有自己的文件夹
-
-# 配置 SSH 目录和权限
+# 创建用户的 .ssh 目录并设置权限
 sudo mkdir -p /usr/local/$USERNAME/.ssh
 echo "$PUB_KEY" | sudo tee /usr/local/$USERNAME/.ssh/authorized_keys > /dev/null
 sudo chmod 700 /usr/local/$USERNAME/.ssh
@@ -39,6 +36,13 @@ sudo bash -c "echo 'Match User $USERNAME' >> /etc/ssh/sshd_config"
 sudo bash -c "echo '    ChrootDirectory /usr/local/$USERNAME' >> /etc/ssh/sshd_config"
 sudo bash -c "echo '    ForceCommand internal-sftp' >> /etc/ssh/sshd_config"
 sudo bash -c "echo '    AllowTcpForwarding no' >> /etc/ssh/sshd_config"
+
+# 确保用户在 Chroot 环境下可以执行基本命令
+# 拷贝必要的命令到 chroot 目录
+sudo mkdir -p /usr/local/$USERNAME/bin
+sudo ln -s /bin/bash /usr/local/$USERNAME/bin/bash
+sudo ln -s /bin/ls /usr/local/$USERNAME/bin/ls
+sudo ln -s /bin/cp /usr/local/$USERNAME/bin/cp
 
 # 重新加载 SSH 配置
 sudo systemctl restart sshd
